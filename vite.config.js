bf8e11cd65a1ff63fb62cf5be788280d614d1531
x4shr <loeric0330@gmail.com>
2026-05-19 12:24:57 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // base: './' 讓它部署在 GitHub Pages 的子路徑時也能正常讀取資源
 export default defineConfig({
-  base: './',
+  base: '/twopay/',
   plugins: [react()],
 })
